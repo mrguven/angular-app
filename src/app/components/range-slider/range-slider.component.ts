97534f60ebd61dc35b8ed1d2641e7826eff84599
mrguven <mrguven@gmail.com>
2023-12-15 00:20:57 +0100

    update range

--- src/app/components/range-slider/range-slider.component.ts
+++ src/app/components/range-slider/range-slider.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import {MatSliderModule} from '@angular/material/slider';
+import {MatInputModule} from '@angular/material/input';
 @Component({
   selector: 'app-range-slider',
   standalone: true,
-  imports: [MatSliderModule],
+  imports: [MatSliderModule,MatInputModule],
   templateUrl: './range-slider.component.html',
   styleUrl: './range-slider.component.css'
 })
