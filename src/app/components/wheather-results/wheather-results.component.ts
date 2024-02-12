c89547a49b6a3e4fff6f1be0634bed1c53ce86e4
mrguven <mrguven@gmail.com>
2024-02-12 22:43:32 +0100

    update

--- src/app/components/wheather-results/wheather-results.component.ts
+++ src/app/components/wheather-results/wheather-results.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-wheather-results',
@@ -8,5 +9,23 @@ import { Component } from '@angular/core';
   styleUrl: './wheather-results.component.css'
 })
 export class WheatherResultsComponent {
+
+generatedNumber!:Number;
+
+getNumber(){
+  let obser = new Observable<Number> (el=> el.next(Math.random()))
+
+obser.subscribe(result=>this.generatedNumber=result)
+console.log(this.generatedNumber);
+}
+
+changeNumber(){
+let newNumber= +this.generatedNumber * 10000000000000000
+this.generatedNumber=newNumber
+}
+
+  
+
+
 
 }
